362b309fd647f42fb4aaad24e289384b74c16902
sneha <sneha.meshram@cgi.com>
2019-08-03 12:52:07 +0530

    Variable Class added

---
+++ src/main/java/com/stackroute/Operators.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+public class Operators {
+    public void main(String[] args) {
+
+        int month1 = 0;
+        int month2 = 0;
+
+        boolean result = (month2 == 29) && month1 == 2014;
+        System.out.println(result);
+
+    }
+}
+
