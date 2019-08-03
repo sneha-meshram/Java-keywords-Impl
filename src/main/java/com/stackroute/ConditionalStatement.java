993d72590a7b6527327796b09be4a7442580775e
sneha <sneha.meshram@cgi.com>
2019-08-03 10:14:53 +0530

    repo created for java-keywords impl

---
+++ src/main/java/com/stackroute/ConditionalStatement.java
@@ -0,0 +1,33 @@
+package com.stackroute;
+
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 600;
+        int c = 500;
+
+        //check among three numbers which is greater.
+
+        if (a> b && a > c)
+            System.out.println("a is largest.");
+        else if (b > a && b > c)
+            System.out.println("b is largest.");
+        else if (c > a && c > b)
+            System.out.println("c is largest.");
+        else
+            System.out.println("The numbers are not distinct.");
+
+
+        int number =10 ;
+
+        // check till number is less than 15
+        while (number < 15)
+        {
+            System.out.println("the numbers are:" + number);
+
+            number++;
+        }
+    }
+
+}
